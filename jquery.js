3f17b5cac1cb437ea571f31c089689699cd9c07f
jonwilami323 <jon_wilson@byu.edu>
2017-10-05 12:09:36 +0000

    Organizing code and annotating

--- jquery.js
+++ jquery.js
@@ -1,29 +1,50 @@
 /*jquery.js*/
 var APIKEY = "5ada51d453098152d4a449d329ff2679";//When we get one
+
+function createHistory(){
+	var symbol = "TSLA";//TSLA, GOOGL, AMZN
+	var startDate = "20161004000000"
+	var type = "daily";
+	var urlHistory ='http://marketdata.websol.barchart.com/getHistory.jsonp?apikey='+APIKEY+'&symbol='+symbol+'&type='+type+'&startDate='+startDate
+	$.ajax({
+	    url : urlHistory,
+	    crossDomain: true,
+	    dataType : "jsonp",
+	    success : function(data) {
+		$("#progress_history").slideUp();
+		//history(data);
+	      console.log(data);
+	    }
+	  });
+}
+
+function createQuote(){
+	var urlQuote ='http://marketdata.websol.barchart.com/getQuote.jsonp?apikey='+APIKEY+'&symbols=TSLA,GOOGL,AMZN'
+	$.ajax({
+	    url : urlQuote,
+	    crossDomain: true,
+	    dataType : "jsonp",
+	    success : function(data) {
+		$("#progress_quote").slideUp();
+		quote(data);
+	      console.log(data);
+	    }
+	  });
+
+}
+
 $(document).ready(function(){
-var symbol = "TSLA";//TSLA, GOOGL, AMZN
-var type = "daily";
-var startDate = "20161004000000"
-//Place JQ here
-var urlHistory ='http://marketdata.websol.barchart.com/getHistory.jsonp?apikey='+APIKEY+'&symbol='+symbol+'&type='+type+'&startDate='+startDate
-var urlQuote ='http://marketdata.websol.barchart.com/getQuote.jsonp?apikey='+APIKEY+'&symbols=TSLA,GOOGL,AMZN'
-$.ajax({
-    url : urlQuote,
-    crossDomain: true,
-    dataType : "jsonp",
-    success : function(data) {
-	$("#progress_quote").slideUp();
-      console.log(data);
-    }
+	createQuote();
   });
-  });
+
+//Build the Quote chart
 var quoteChart = new Chart($("#quoteChart"), {
         type: "bar",
         data: {
-	    labels: [],
+               labels: ["TSLA","GOOGL","AMZN"],
             datasets: [{
+		labe: 'data',
                 data: [],
-                labels: ["TSLA","GOOGL","AMZN"],
                 backgroundColor:[ 
                     "rgba(255, 99, 132, .5)",
                     "rgba(0,255,0, .5)",
@@ -52,15 +73,18 @@ var quoteChart = new Chart($("#quoteChart"), {
             }
         }
     });
+
+//Parse the data for the chart
+//Quote parsing data
 var quote = function(data){
     var high = []
     var name = []
         for(j = 0; j < data.results.length; j++){
-            high.push(data.results[i].high)
-            name.push(data.results[i].name)
+            high.push(data.results[j].high)
+            name.push(data.results[j].name)
+            quoteChart.data.datasets.data.push(data.results[j].high);
+            quoteChart.data.labels.push(data.results[j].name);
         }
-        quoteChart.data.datasets.data = high;
-    //quoteChart.data.labels = name;
-    quoteChart.update();
+    	    quoteChart.update();
 };
 
